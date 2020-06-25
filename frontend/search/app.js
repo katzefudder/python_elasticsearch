fe04cfe0c6333f6efc3285d0b94fe9d1a09d6d44
Florian Dehn <flo@katzefudder.de>
2020-06-25 11:16:22 +0200

    show all results when page loaded

--- frontend/search/app.js
+++ frontend/search/app.js
@@ -12,17 +12,54 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname+'/index.html'));
 });
 
-// Route to search for Articles by title
 app.get('/search/:title', function (req, res) {
-    // Access title like this: req.params.title
-    /* Query using slop to allow for unexact matches */
-    client.search({
+        doFuzzySearch(req.params.title)
+    .then(function(resp) {
+        console.log("Fuzzy Search returned:", resp);
+        res.send(resp);
+    }, function(err) {
+        console.trace(err.message);
+        res.send(err.message);
+    });
+});
+
+app.get('/getAllPictures', function (req, res) {
+    getAllPictures()
+    .then(function(resp) {
+        console.log("Search All returned:", resp);
+        res.send(resp);
+    }, function(err) {
+        console.trace(err.message);
+        res.send(err.message);
+    });
+});
+
+// Start listening for requests on port 3000
+app.listen(8080, function () {
+  console.log('App listening for requests...');
+});
+
+function getAllPictures() {
+    return client.search({
+        index: 'galleries',
+            "size" : 10000,
+            body: {
+                "query": {
+                    "match_all" : {}
+                },
+                "_source": ["headline", "caption", "keywords", "filename"]
+            }
+    });
+}
+
+function doFuzzySearch(query) {
+    return client.search({
         index: 'galleries',
         size: 10,
         body: {
             "query": {
                 "multi_match" : {
-                    "query" : req.params.title,
+                    "query" : query,
                     "fields": ["headline", "caption", "keywords", "filename"],
                     "fuzziness": "AUTO"
                 }
@@ -30,16 +67,5 @@ app.get('/search/:title', function (req, res) {
             "_source": ["headline", "caption", "keywords", "filename"],
             "size": 10
         }
-    }).then(function(resp) {
-        console.log("Successful query! Here is the response:", resp);
-        res.send(resp);
-    }, function(err) {
-        console.trace(err.message);
-        res.send(err.message);
     });
-  });
-
-// Start listening for requests on port 3000
-app.listen(8080, function () {
-  console.log('App listening for requests...');
-});
+}
--- frontend/search/public/js/index.js
+++ frontend/search/public/js/index.js
@@ -12,49 +12,58 @@ $(document).ready(function () {
             url: "/search/" + title,
             type: "GET"
           });
+        },
+        getAllPictures: function() {
+          return $.ajax({
+            url: "/getAllPictures/",
+            type: "GET"
+          });
         }
-      };
+    };
     
     var handleSubmitTitleSearch = function(event) {
         event.preventDefault();
-    
         var searchTerm = $titleSearchText.val().trim();
-    
         searchAPI.searchByTitle(searchTerm).then(function(resp) {
-    
-            // https://presspix.s3.eu-central-1.amazonaws.com/demo_customer/images/gallery_1/20200605_ECBN_Unterstützer_010.jpg
-            var data = [];
-            data[0] = ["Foto", "Galerie", "Beschreibung", "Keywords"];
-            var hitsArray = resp.hits.hits;
-            hitsArray.forEach(function(photos) {
-              var imageUrl = photos._source['filename'].replace('/tmp/s3/presspix/', '');
-              imageUrl = "https://presspix.s3.eu-central-1.amazonaws.com/" + imageUrl;
-              data.push([imageUrl, photos._source['headline'], photos._source['caption'], photos._source['keywords']]);
-            });
-    
-              var photosTable = makeTable($("#tableDiv"), data);
+          console.log(resp);
+            var results = handleResults(resp.hits.hits);        
+            var photosTable = makeTable($("#tableDiv"), results);
         });
-      
-        // Clear out search field
-        $submitTitleSearchButton.val("");
-      };
-    
-      function makeTable(container, data) {
-        var table = $("<table/>").addClass('table table-striped');
-        $.each(data, function(rowIndex, r) {
-            var row = $("<tr/>");
-            $.each(r, function(colIndex, c) { 
-                if (colIndex == 0 && rowIndex > 0) {
-                  row.append($("<img style='width: auto; height: 100px' src='"+c+"' />"));
-                } else {
-                  row.append($("<t"+(rowIndex == 0 ?  "h" : "d")+"/>").text(c));
-                }
-            });
-            table.append(row);
-        });
-        return container.html(table);
-    }
+    };
+
+    searchAPI.getAllPictures().then(function(resp) {
+      var results = handleResults(resp.hits.hits);        
+      var photosTable = makeTable($("#tableDiv"), results);
+    });
     
     // Add event listeners to the submit button
     $submitTitleSearchButton.on("click", handleSubmitTitleSearch);
+  });
+
+  function handleResults(results) {
+    var data = [];
+    data[0] = ["Foto", "Galerie", "Beschreibung", "Keywords"];
+    var hitsArray = results;
+    hitsArray.forEach(function(photos) {
+      var imageUrl = photos._source['filename'].replace('/tmp/s3/presspix/', '');
+      imageUrl = "https://presspix.s3.eu-central-1.amazonaws.com/" + imageUrl;
+      data.push([imageUrl, photos._source['headline'], photos._source['caption'], photos._source['keywords']]);
     });
+    return data;
+  }
+
+  function makeTable(container, data) {
+    var table = $("<table/>").addClass('table table-striped');
+    $.each(data, function(rowIndex, r) {
+        var row = $("<tr/>");
+        $.each(r, function(colIndex, c) { 
+            if (colIndex == 0 && rowIndex > 0) {
+              row.append($("<td><a href='"+c+"' download><img style='width: auto; height: 100px' src='"+c+"' /></a></td>"));
+            } else {
+              row.append($("<t"+(rowIndex == 0 ?  "h" : "d")+"/>").text(c));
+            }
+        });
+        table.append(row);
+    });
+    return container.html(table);
+  }
